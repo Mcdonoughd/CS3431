a67a9d421b34b3a7bfc04f789303e1a094300f56
Mcdonoughd <dmcdonough@wpi.edu>
2018-02-08 13:23:47 -0500

    Phase 1

--- Team32.sql
+++ Team32.sql
@@ -1,8 +1,8 @@
 /*Phase 2 By Daniel McDonough & Talal Jaber*/
 /*desc tablename : This allows you to see the status of a query*/
-
-
 /*Part 1 CREATE TABLE*/
+
+
 
 /*Employee*/
 CREATE TABLE Employee(
@@ -14,13 +14,14 @@ CREATE TABLE Employee(
 	OfficeNum CHAR(30) NOT NULL UNIQUE, /*Assume: B16 is an office, Employees cannot share office space */
 	EmpRank INTEGER NOT NULL, /* employee is regular (rank = 0), division manager (rank = 1), or general manager (rank = 2) */
 	SupervisorID INTEGER, /*General Managers dont have supervisors*/
-	CONSTRAINT CHK_EmpRank CHECK (EmpRank == '0' OR EmpRank == '1' OR EmpRank == '2')
+	CONSTRAINT CHK_EmpRank CHECK (EmpRank = 0 OR EmpRank = 1 OR EmpRank = 2)
 );
 
 /*Room */
 CREATE TABLE Room(
 	Num INTEGER NOT NULL PRIMARY KEY,
-	Occupied BOOLEAN
+	Occupied CHAR(1),
+	CONSTRAINT CHK_Occupied CHECK (Occupied = 0 OR Occupied = 1)
 ); 
 
 /*Equipment */
@@ -38,26 +39,25 @@ CREATE TABLE Equipment(
 /*EquipmentType */
 CREATE TABLE EquipmentType (
 	ID INTEGER NOT NULL PRIMARY KEY,
-	Description DESCRIBE, /*Who needs instructions? - can be NULL*/
+	Description CHAR(20), /*Who needs instructions? - can be NULL*/
 	Model VARCHAR2(20) NOT NULL UNIQUE,
 	Instructions CHAR(500), /*Who needs instructions? - can be NULL*/
 	FOREIGN KEY (ID) REFERENCES Equipment(TypeID)
 );
 
-
-
 /*RoomService */
 CREATE TABLE RoomService(
 	RoomNum INTEGER NOT NULL PRIMARY KEY,
-	Service CHAR(20) NOT NULL PRIMARY KEY,
+	Service CHAR(20) NOT NULL UNIQUE,
 	FOREIGN KEY (RoomNum) REFERENCES Room(Num)
 );
 
 /*RoomAccess */
 CREATE TABLE RoomAccess(
-	RoomNum INTEGER NOT NULL PRIMARY KEY,
-	EmpID INTEGER NOT NULL PRIMARY KEY,
-	FOREIGN KEY (RoomNum) REFERENCES Room(Num)
+	RoomNum INTEGER NOT NULL,
+	EmpID INTEGER NOT NULL,
+	FOREIGN KEY (RoomNum) REFERENCES Room(Num),
+	FOREIGN KEY (EmpID) REFERENCES Employee(ID)
 );
 
 /*Patient */
@@ -74,39 +74,68 @@ CREATE TABLE Doctor(
 	ID INTEGER NOT NULL PRIMARY KEY,
 	FirstName CHAR(20) NOT NULL,
 	LastName CHAR(30) NOT NULL,
-	Gender BOOLEAN,
-	Specialty CHAR(20) NOT NULL /*ASSUME SPECIALTY CAN BE LABLED AS GENERAL*/
+	Gender CHAR(1),
+	Specialty CHAR(20) NOT NULL, /*ASSUME SPECIALTY CAN BE LABLED AS GENERAL*/
+	CONSTRAINT CHK_Gender CHECK (Gender = 'M' OR Gender = 'F')
 );
 
 /*Admission */
 CREATE TABLE Admission(
-	Num INTEGER NOT NULL PRIMARY KEY,
+	ANum INTEGER NOT NULL PRIMARY KEY,
 	AdmissionDate DATE NOT NULL,
 	LeaveDate DATE, /*Assume leave date is not pre-scheduled*/
 	TotalPayment REAL NOT NULL,
 	InsurancePayment REAL, /*Assume patient can have null insurance*/
 	Patient_SSN INTEGER NOT NULL,
 	FutureVisit Date, /*Patient doesnt need to have a future visit*/
-	FOREIGN KEY Patient_SSN REFERENCES Patient(SSN)
+	FOREIGN KEY (Patient_SSN) REFERENCES Patient(SSN)
 );
 
 /*Examine*/
 CREATE TABLE Examine(
-	DoctorID INTEGER NOT NULL PRIMARY KEY,
-	AdmissionNum INTEGER NOT NULL PRIMARY KEY,
-	Comment CHAR(200),
-	FOREIGN KEY DoctorID REFERENCES Doctor(ID),
-	FOREIGN KEY AdmissionNum REFERENCES Admission(Num)
+	DoctorID INTEGER NOT NULL,
+	AdmissionNum INTEGER NOT NULL,
+	Comments CHAR(200),
+	FOREIGN KEY (DoctorID) REFERENCES Doctor(ID),
+	FOREIGN KEY (AdmissionNum) REFERENCES Admission(ANum)
 );
 
 /*StayIn*/
 CREATE TABLE StayIn(
-	AdmissionNum INTEGER NOT NULL PRIMARY KEY,
-	RoomNum INTEGER NOT NULL PRIMARY KEY,
+	AdmissionNum INTEGER NOT NULL UNIQUE,
+	RoomNum INTEGER NOT NULL UNIQUE,
 	StartDate DATE NOT NULL PRIMARY KEY,
 	EndDate DATE,
-	FOREIGN KEY RoomNum REFERENCES Room(Num),
-	FOREIGN KEY AdmissionNum REFERENCES Admission(Num)
+	FOREIGN KEY (RoomNum) REFERENCES Room(Num),
+	FOREIGN KEY (AdmissionNum) REFERENCES Admission(ANum)
 );
+
+/*Begin Population!*/
+
+
+
+
+
+
+
+
+/*DROP TABLES TO MAKE SURE EVERYTHING IS HUNKYDORY*/
+DROP TABLE Employee CASCADE CONSTRAINTS;
+DROP TABLE Room CASCADE CONSTRAINTS;
+DROP TABLE Equipment CASCADE CONSTRAINTS;
+DROP TABLE EquipmentType CASCADE CONSTRAINTS;
+DROP TABLE RoomAccess CASCADE CONSTRAINTS;
+DROP TABLE RoomService CASCADE CONSTRAINTS;
+DROP TABLE Patient CASCADE CONSTRAINTS;
+DROP TABLE Doctor CASCADE CONSTRAINTS;
+DROP TABLE Admission CASCADE CONSTRAINTS;
+DROP TABLE Examine CASCADE CONSTRAINTS;
+DROP TABLE StayIn;
+
+
+
+
+
+
 
 
