81cc384038790404083f6799a1487ba4b960457f
Mcdonoughd <dmcdonough@wpi.edu>
2018-02-25 21:50:49 -0500

    Fixed first trigger

--- phase3.sql
+++ phase3.sql
@@ -1,5 +1,5 @@
 /*Phase 3 By Team 32 (Daniel McDonough & Talal Jaber)*/
-
+set serveroutput on;
 DROP VIEW CriticalCases;
 DROP VIEW DoctorsLoad;
 DROP TRIGGER CheckRoomServiceCount;
@@ -76,14 +76,15 @@ Where C.Patient_SSN = A.Patient_SSN
 
 /* Any room in the hospital cannot offer more than three services. */
 
-CREATE TRIGGER CheckRoomServiceCount 
+CREATE OR REPLACE TRIGGER CheckRoomServiceCount 
  BEFORE INSERT OR UPDATE 
  ON RoomService
  Declare 
       serviceCount int;
 Begin
-      Select count(Service)  into serviceCount
-	  FROM RoomService;
+      Select count(Service) into serviceCount
+	  FROM RoomService
+	  WHERE RoomNum = :new.RoomNum;
      IF serviceCount > 3  Then
 	 RAISE_APPLICATION_ERROR(-20004, 'A Room Cannot Have more than 3  Services');
 	 END IF;
@@ -94,7 +95,7 @@ END;
 /*The insurance payment should be calculated automatically as 70% of the total
 payment. If the total payment changes then the insurance amount should also
 change.*/
-CREATE TRIGGER CalcInsurance
+CREATE OR REPLACE TRIGGER CalcInsurance
 BEFORE INSERT OR UPDATE
 ON Admission
 FOR EACH ROW
@@ -107,7 +108,7 @@ END;
 division managers (with rank 1). Also each regular employee must have a
 supervisor at all times. */
 /*check if supervisor rank is not 1 or null*/
-CREATE TRIGGER CHECKMANAGERS
+CREATE OR REPLACE TRIGGER CHECKMANAGERS
 BEFORE INSERT OR UPDATE 
 ON Employee
 FOR EACH ROW
@@ -124,7 +125,7 @@ END;
 
 
 /* Before you delete a supervisor check if they have employees*/
-CREATE TRIGGER CHECKSUPERVISEE
+CREATE OR REPLACE TRIGGER CHECKSUPERVISEE
 BEFORE DELETE 
 ON Employee 
 FOR EACH ROW 
@@ -141,7 +142,7 @@ END;
 
 /* Similarly, division managers (with rank 1) must have their supervisors as general
 managers (with rank 2). Division managers must have supervisors at all times.*/
-CREATE TRIGGER CHECKGENMANAGERS
+CREATE OR REPLACE TRIGGER CHECKGENMANAGERS
 BEFORE INSERT OR UPDATE 
 ON Employee
 FOR EACH ROW
@@ -158,7 +159,7 @@ END;
 
 
 /* Before you delete a supervisor check if they have employees*/
-CREATE TRIGGER CHECKDIVSUPERVISEE
+CREATE OR REPLACE TRIGGER CHECKDIVSUPERVISEE
 BEFORE DELETE 
 ON Employee 
 FOR EACH ROW 
@@ -178,7 +179,7 @@ automatically set to 3 months after that date, i.e., D + 3 months. The
 futureVisitDate may be manually changed later, but when the ICU admission
 happens, the date should be set to default as mentioned above. */
 
-CREATE TRIGGER CHECKFutureVisit
+CREATE OR REPLACE TRIGGER CHECKFutureVisit
 BEFORE INSERT 
 ON Admission
 FOR EACH ROW
@@ -195,11 +196,11 @@ IF(RmType='ICU')THEN
 END IF;
 END;
 /
-Show errors;
+
 
 /* If an equipment of type ‘MRI’, then the purchase year must be not null and after
 2005.*/
-CREATE TRIGGER MRI2005CHECK
+CREATE OR REPLACE TRIGGER MRI2005CHECK
 BEFORE INSERT OR UPDATE 
 ON Equipment 
 FOR EACH ROW
@@ -211,25 +212,28 @@ END IF;
 END;
 /
 
-
 /* When a patient is admitted to the hospital, i.e., a new record is inserted into the
 Admission table; the system should print out the names of the doctors who
 previously examined this patient (if any).*/
-CREATE TRIGGER PREVDOC
-AFTER INSERT 
+CREATE OR REPLACE TRIGGER PREVDOC
+Before INSERT 
 ON Admission 
 FOR EACH ROW 
-
+DECLARE
+	FullName varchar2(40);
 BEGIN
-SELECT DISTINCT D.FirstName,D.LastName INTO TempDoc
+SELECT  CONCAT(D.FirstName, ', ', D.LastName) AS DocName INTO FullName
 FROM Doctor D,Examine E,Admission A
 WHERE A.ANum=E.AdmissionNum
-AND E.DoctorID=D.ID
-AND A.Patient_SSN =:new.Patient_SSN;
+AND E.DoctorID = D.ID
+AND A.Patient_SSN = :new.Patient_SSN;
+IF (FullName != NULL) THEN
+
+END IF;
 END;
 /
 Show errors;
-
+/*dbms_output.put_line('Doctor: '||FullName);*/
 
 
 
