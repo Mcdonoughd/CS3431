bb882f798f5bd80439dd821dc2b3e9746fba6230
Mcdonoughd <dmcdonough@wpi.edu>
2018-02-25 20:20:00 -0500

    OH IT

--- phase3.sql
+++ phase3.sql
@@ -1,5 +1,16 @@
 /*Phase 3 By Team 32 (Daniel McDonough & Talal Jaber)*/
-/*desc tablename : This allows you to see the status of a query*/
+
+DROP VIEW CriticalCases;
+DROP VIEW DoctorsLoad;
+DROP TRIGGER CheckRoomServiceCount;
+DROP TRIGGER CalcInsurance;
+DROP TRIGGER CHECKMANAGERS;
+DROP TRIGGER CHECKDIVSUPERVISEE;
+DROP TRIGGER CHECKGENMANAGERS;
+DROP TRIGGER CHECKSUPERVISEE;
+DROP TRIGGER CHECKFutureVisit;
+DROP TRIGGER MRI2005CHECK;
+DROP TRIGGER PREVDOC;
 
 
 /* PHASE 3 START* /
@@ -17,7 +28,7 @@ CREATE VIEW CriticalCases AS
                 From RoomService 
                 Where Service = 'ICU')) 
         Group By Patient_SSN) 
-    Where numberOfAdmissionsToICU >= 2;/
+    Where numberOfAdmissionsToICU >= 2;
 
 
 CREATE VIEW DoctorsLoad AS    
@@ -33,8 +44,10 @@ CREATE VIEW DoctorsLoad AS
             Select DoctorID AS ID, Count(AdmissionNum) AS LoadNum
             From Examine
             Group By DoctorID)
-        Where Loadnum <= 10);/
+        Where Loadnum <= 10);
 
+CREATE VIEW 		
+		
 
 Select *
 From CriticalCases
@@ -63,21 +76,20 @@ Where C.Patient_SSN = A.Patient_SSN
 /*Part 2 TRIGGERS*/
 
 /* Any room in the hospital cannot offer more than three services. */
+
 CREATE TRIGGER CheckRoomServiceCount 
  BEFORE INSERT OR UPDATE 
- ON RoomService  
+ ON RoomService
  Declare 
       serviceCount int;
 Begin
       Select count(Service)  into serviceCount
-      COUNT(Service), roomNum
-	  FROM RoomService
-	  GROUP BY roomNum;
-      
-     IF  serviceCount > 3  Then
+	  FROM RoomService;
+     IF serviceCount > 3  Then
 	 RAISE_APPLICATION_ERROR(-20004, 'A Room Cannot Have more than 3  Services');
 	 END IF;
-END; /
+END; 
+/
 
 
 /*The insurance payment should be calculated automatically as 70% of the total
@@ -86,106 +98,136 @@ change.*/
 CREATE TRIGGER CalcInsurance
 BEFORE INSERT OR UPDATE
 ON Admission
+FOR EACH ROW
 BEGIN
 :new.InsurancePayment := :new.TotalPayment * 0.70;
-END;/
+END;
+/
 
 /*Ensure that regular employees (with rank 0) must have their supervisors as
 division managers (with rank 1). Also each regular employee must have a
 supervisor at all times. */
 /*check if supervisor rank is not 1 or null*/
 CREATE TRIGGER CHECKMANAGERS
-BEFORE INSERT OR UPDATE ON Employees
-BEGIN
-	SELECT EmpID, EmpRank as BossRank
-	FROM (SELECT EmpRank, EmpID, SupervisorID as BossID /*get supervisor id*/
-	FROM Employees 
-	Where :new.EmpID = EmpID && EmpRank = 0) 
-	Where EmpID = BossID
-	IF BossRank NOT = 1 OR BossRank = NULL then	
-	RAISE_APPLICATION_ERROR(-20004, 'Employee has invalid supervisor');
-	END IF
-END;/
+BEFORE INSERT OR UPDATE 
+ON Employee
+FOR EACH ROW
+WHEN (new.EmpRank = 0)
+DECLARE
+   BossRank NUMBER := 1;
+BEGIN 
+    Select EmpRank INTO BossRank FROM Employee WHERE ID = :new.SupervisorID;
+    IF(BossRank != 1) THEN
+	RAISE_APPLICATION_ERROR(-20004, 'Employee of Rank 0 must have Supervisor of Rank 1');
+	END IF;
+END;
+/
+
 
 /* Before you delete a supervisor check if they have employees*/
 CREATE TRIGGER CHECKSUPERVISEE
-BEFORE DELETE ON Employees
+BEFORE DELETE 
+ON Employee 
+FOR EACH ROW 
+WHEN (old.EmpRank = 1)
+DECLARE	
+	SUPERVISEE INTEGER := 0;
 BEGIN 
-	SELECT count(EmpID) as supervided, SupervisorID From Employees
-	Where :new.EmpRank = 0
-	Group By SupervisorID
-	IF supervided > 0 then
-	RAISE_APPLICATION_ERROR(-200,'THE SUPERVISOR IS STILL SUPERVISING PEOPLE!');
-	END IF
-END;/
-
+	SELECT COUNT(ID) INTO SUPERVISEE FROM Employee WHERE SupervisorID = :old.ID;
+	IF SUPERVISEE > 0 then
+	RAISE_APPLICATION_ERROR(-200,'This Person is still supervising people. Cannot Be Removed.');
+	END IF;
+END;
+/
 
 /* Similarly, division managers (with rank 1) must have their supervisors as general
 managers (with rank 2). Division managers must have supervisors at all times.*/
 CREATE TRIGGER CHECKGENMANAGERS
-BEFORE INSERT OR UPDATE ON Employees
-BEGIN
-	SELECT EmpID, EmpRank as BossRank
-	FROM (SELECT EmpRank, EmpID, SupervisorID as BossID /*get supervisor id*/
-	FROM Employees 
-	Where :new.EmpID = EmpID && EmpRank = 1) 
-	Where EmpID = BossID;
-	IF BossRank NOT = 2 OR BossRank = NULL then	
-	RAISE_APPLICATION_ERROR(-20004, 'Employee has invalid supervisor');
-	END IF
-END;/
+BEFORE INSERT OR UPDATE 
+ON Employee
+FOR EACH ROW
+WHEN (new.EmpRank = 1)
+DECLARE
+   BossRank NUMBER := 2;
+BEGIN 
+    Select EmpRank INTO BossRank FROM Employee WHERE ID = :new.SupervisorID;
+    IF(BossRank != 2) THEN
+	RAISE_APPLICATION_ERROR(-20004, 'Employee of Rank 1 must have Supervisor of Rank 2');
+	END IF;
+END;
+/
+
 
 /* Before you delete a supervisor check if they have employees*/
 CREATE TRIGGER CHECKDIVSUPERVISEE
-BEFORE DELETE ON Employees
+BEFORE DELETE 
+ON Employee 
+FOR EACH ROW 
+WHEN (old.EmpRank = 2)
+DECLARE	
+	SUPERVISEE INTEGER := 0;
 BEGIN 
-	SELECT count(EmpID) as supervided, SupervisorID From Employees
-	Where :new.EmpRank = 1
-	Group By SupervisorID
-	IF supervided > 0 then
-	RAISE_APPLICATION_ERROR(-200,'THE SUPERVISOR IS STILL SUPERVISING PEOPLE!');
-	END IF
-END;/
-	
+	SELECT COUNT(ID) INTO SUPERVISEE FROM Employee WHERE SupervisorID = :old.ID;
+	IF SUPERVISEE > 0 then
+	RAISE_APPLICATION_ERROR(-200,'This Person is still supervising people. Cannot Be Removed.');
+	END IF;
+END;
+/	
+
 /* When a patient is admitted to ICU room on date D, the futureVisitDate should be
 automatically set to 3 months after that date, i.e., D + 3 months. The
 futureVisitDate may be manually changed later, but when the ICU admission
 happens, the date should be set to default as mentioned above. */
 
 CREATE TRIGGER CHECKFutureVisit
-AFTER INSERT ON Admission
+BEFORE INSERT ON Admission
+FOR EACH ROW
 BEGIN
-	:old.FutureVisit := :old.AdmissionDate + INTERVAL '3' Month(1);
-END;/
+	:new.FutureVisit := :new.AdmissionDate + INTERVAL '3' Month;
+END;
+/
+
 
 /* If an equipment of type ‘MRI’, then the purchase year must be not null and after
 2005.*/
 CREATE TRIGGER MRI2005CHECK
-AFTER INSERT OR UPDATE ON Equipment
+BEFORE INSERT OR UPDATE 
+ON Equipment 
+FOR EACH ROW
+WHEN (new.TypeID = 'MRI') 
 BEGIN
-SELECT PurchaseYear,TypeID FROM Equipment
-IF TypeID = 'MRI' AND (PurchaseYear > TO_DATE('2005') OR PurchaseYear = NULL)then
+IF (:new.PurchaseYear>TO_DATE('2005') OR :new.PurchaseYear=NULL) THEN
 RAISE_APPLICATION_ERROR(-300,'MRI Machine can not be purchased after 2005 or null');
-END IF
-END;/
+END IF;
+END;
+/
+
 
 /* When a patient is admitted to the hospital, i.e., a new record is inserted into the
 Admission table; the system should print out the names of the doctors who
 previously examined this patient (if any).*/
-CREATE TRIGGER
-AFTER INSERT ON Admission
+CREATE TRIGGER PREVDOC
+BEFORE INSERT 
+ON Admission 
+FOR EACH ROW 
+DECLARE	
+	FNAME VARCHAR(20);
+	LNAME VARCHAR(20);
 BEGIN
-SELECT DISTINCT FirstName,LastName 
-FROM Examine NATURAL Join Doctor NATURAL JOIN Admission 
-WHERE :old.Patient_SSN = Patient_SSN
-Group BY Patient_SSN
-END;/
+SELECT FirstName INTO FNAME
+FROM Doctor as D 
+Where D.ID = (SELECT DISTINCT DoctorID 
+FROM Examine as E 
+WHERE E.AdmissionNum = ( SELECT ANum 
+FROM Admission as A 
+WHERE A.Patient_SSN = :new.Patient_SSN));
+IF (FNAME != NULL AND LNAME != NULL) THEN
+RAISE_APPLICATION_ERROR(-300,'OH HI');
 
-
-
-
-
-
+END IF;
+END;
+/
+Show errors;
 
 
 
